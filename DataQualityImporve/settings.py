96c25396894229ca303335a090548e43580d7976
Murali Krishna Mopidevi <muralikrishna.mopidevi@gmail.com>
2020-03-13 11:39:53 +0100

    settings updated

--- DataQualityImporve/settings.py
+++ DataQualityImporve/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+
 ALLOWED_HOSTS = ['*']
 
 IMPORT_EXPORT_USE_TRANSACTIONS = False
