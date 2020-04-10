2312fdf7976efba42913859c32aaea5e2ed035ac
Murali Krishna Mopidevi <muralikrishna.mopidevi@gmail.com>
2020-04-10 02:03:59 +0200

    Preprocess-2

--- dash/views.py
+++ dash/views.py
@@ -1,9 +1,9 @@
 import os
 import plotly.graph_objs as go
 import plotly.offline as opy
-from dash.models import CurrentFile,Prepross
-from django.shortcuts import render, redirect, reverse
-from django.views.generic import TemplateView, ListView, CreateView
+from dash.models import CurrentFile, Prepross
+from django.shortcuts import redirect
+from django.views.generic import TemplateView, CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
@@ -16,9 +16,8 @@ from .forms import CSVForm
 from .models import CSV
 
 
-
-
-
+# ------------------ UPLOADING CSV FILE ------------------------- #
+@login_required
 class UploadBookView(CreateView):
     model = CSV
     form_class = CSVForm
@@ -26,12 +25,14 @@ class UploadBookView(CreateView):
     template_name = 'dash/upload.html'
 
 
+# ------------------ SELECTING CSV FILE ------------------------- #
 class index(TemplateView):
     template_name = 'dash/index.html'
 
-    def post(self, request, **kwargs):
+    @staticmethod
+    def post(request, **kwargs):
         if request.method == 'POST':
-            current_file = CurrentFile(filename = list(request.POST.keys())[1])
+            current_file = CurrentFile(filename=list(request.POST.keys())[1])
             current_file.save()
             return redirect('prepross')
         else:
@@ -41,76 +42,94 @@ class index(TemplateView):
         return self.template_name
 
 
-# *****************************DATA PREPROCESSING*********************************************
-
+# ------------------ PREPROCESSING  ------------------------- #
+@login_required
 class prepross(TemplateView):
     template_name = 'dash/preprocessing.html'
-    #model = Prepross
-    #fields = ('file_name','coltype','Xvars','Yvar','onehot','featscaling','na_omit','ordinal')
 
     def get_context_data(self, **kwargs):
-        data = super().get_context_data(**kwargs)
+        super().get_context_data(**kwargs)
         context = {}
         try:
             # *****************************LOADING DATA FROM MODELS*************************************
-            file_name = CurrentFile.objects.order_by('-id')[0].filename #it gets the uploaded filename from database
-            df = pd.read_csv(os.path.join('Media\csv', file_name)) # it displays uploaded dataset from database
-            count_nan = len(df) - df.count() # it counts the number of missing values
-            row_count = df.count()[0] #it counts the number of rows in the dataset
-            file_type = pd.concat([df.dtypes, count_nan, df.nunique()], axis=1) #concat the total dataset https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.concat.html
-            file_type.columns = ("Type", "NA count", "Count distinct")#displaying details of the dataset
+            # It gets the uploaded filename from database
+            file_name = CurrentFile.objects.order_by('-id')[0].filename
+
+            # It Reads Selected file from database to Pandas Dataframe
+            df = pd.read_csv(os.path.join('Media\csv', file_name))
+
+            # It counts the number of missing values
+            count_nan = len(df) - df.count()
+
+            # It counts the number of rows in the dataset
+            row_count = df.count()[0]
+
+            # concat the total dataset
+            file_type = pd.concat([df.dtypes, count_nan, df.nunique()], axis=1)
+
+            # displaying details of the Dataset
+            file_type.columns = ("Type", "NA count", "Count distinct")
 
             context['file_type'] = file_type
             context['row_count'] = row_count
         except:
             file_name = 'Please select one file'
-
-
         context['file_name'] = file_name
         return context
 
     def post(self, request, **kwargs):
-        if request.method == 'POST': #post the requests
-
-            # Entry to model of the data preprocessing to post the requests
-            # *****************************LOADING DATA FROM MODELS*************************************
-            prep_file = Prepross.objects.get_or_create(filename = CurrentFile.objects.order_by('-id')[0].filename,
-                                                       coltype = request.POST.getlist('coltype'),
-                                                       assvar = request.POST.getlist('assvar'),
-                                                       missingvalues = request.POST.getlist('missingvalues'),
-                                                       trainingset_size = request.POST['trainingset_size'],
-                                                       featscaling = request.POST.getlist('featscaling'),
-                                                       ordinal =  request.POST.getlist('ordinal'),
-
-                                   )
-
-            # Get dataframe and change data type
-            #https://docs.python.org/3/library/functions.html
-            # i used python builtin function to write this function
+        if request.method == 'POST':
+            Prepross.objects.get_or_create(filename=CurrentFile.objects.order_by('-id')[0].filename,
+                                           coltype=request.POST.getlist('coltype'),
+                                           assvar=request.POST.getlist('assvar'),
+                                           missingvalues=request.POST.getlist('missingvalues'),
+                                           trainingset_size=request.POST['trainingset_size'],
+                                           featscaling=request.POST.getlist('featscaling'),
+                                           ordinal=request.POST.getlist('ordinal'), )
             context = {}
+
+            # Selecting File from database
             file_name = CurrentFile.objects.order_by('-id')[0].filename
+
+            # Selecting Column type
             coltype = request.POST.getlist('coltype')
-            coltype = dict([i.split(':', 1)for i in coltype])#which spilts the columns
-            df = pd.read_csv(os.path.join('Media\csv', file_name), dtype= coltype)
+
+            # which spilts the columns
+            coltype = dict([i.split(':', 1) for i in coltype])
+
+            # CSV to pandas dataframe.
+            df = pd.read_csv(os.path.join('Media\csv', file_name), dtype=coltype)
+
             row_count = df.count()[0]
 
             # Keep only selected columns
             assvar = request.POST.getlist('assvar')
-            xcols0 = [s for s in assvar if ":X" in s]# selected x columns
+
+            # selected x columns
+            xcols0 = [s for s in assvar if ":X" in s]
             xcols = [i.split(':', 1)[0] for i in xcols0]
+
+            # selected y columns
             ycol0 = [s for s in assvar if ":y" in s]
-            ycol = [i.split(':', 1)[0] for i in ycol0]#selected y columns
+            ycol = [i.split(':', 1)[0] for i in ycol0]
             cols = xcols + ycol
             df = df[cols]
 
-            xcols = ', '.join(xcols)#joining the x columns
-            ycol = ', '.join(ycol)#joining the y columns
-            missing = request.POST.getlist('missingvalues')#posting the missing values
-            missing = ', '.join(missing)#join()accept any iterable
+            # joining the x columns
+            xcols = ', '.join(xcols)
+
+            # joining the y columns
+            ycol = ', '.join(ycol)
+
+            # Posting the missing values
+            missing = request.POST.getlist('missingvalues')
+
+            # join()accept any iterable
+            missing = ', '.join(missing)
             trainingset_s = request.POST.getlist('trainingset_size')
             trainingset_s = ', '.join(trainingset_s)
             testset_s = 100 - int(trainingset_s)
-            feat =  request.POST['featscaling']
+            feat = request.POST['featscaling']
             encode = request.POST['ordinal']
 
             # Taking care of missing data in the data set
@@ -120,7 +139,6 @@ class prepross(TemplateView):
                 else:
                     df = df.dropna()
 
-
             # Return error if columns are not selected
             if len(ycol0) != 1:
                 context['selecty'] = 'Please select one y variable'
@@ -128,14 +146,8 @@ class prepross(TemplateView):
             elif len(xcols0) < 1:
                 context['selecty'] = 'Please select one or more X variables'
 
-
             else:
-
-                # Ploting thgraphs by using plotly
-                #https://plot.ly/
-
                 graph = {}
-
                 for i in df.columns:
                     layout = go.Layout(autosize=False, width=400, height=400,
 
@@ -150,11 +162,9 @@ class prepross(TemplateView):
                                        bargroupgap=0.1)
 
                     data = go.Figure(data=[go.Histogram(x=df[i])], layout=layout)
-
                     graph[i] = opy.plot(data, include_plotlyjs=False, output_type='div')
-
-                context['graph'] = graph
-            #displaying the selected data
+                    context['graph'] = graph
+            # displaying the selected data
             context['xcols'] = xcols
             context['ycol'] = ycol
             context['missing'] = missing
@@ -164,28 +174,13 @@ class prepross(TemplateView):
             context['encode'] = encode
             context['file_name'] = file_name
             context['row_count'] = row_count
-            return render(request,'dash/preprocessing.html', context)
+            return render(request, 'dash/preprocessing.html', context)
 
     def __str__(self):
         return self.name
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Profile
 @login_required
 def profile(request):
     if request.method == 'POST':
@@ -221,4 +216,3 @@ def change_password(request):
     return render(request, 'dash/password.html', {
         'form': form
     })
-
