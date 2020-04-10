d8034347085bf5d689084ab044dc876662cc5767
Murali Krishna Mopidevi <muralikrishna.mopidevi@gmail.com>
2020-04-10 17:01:26 +0200

    heroku

--- dash/models.py
+++ dash/models.py
@@ -8,7 +8,7 @@ class Profile(models.Model):
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
-        return f'{self.user.username}Profile'
+        return f'{self.user.usename}Profile'
 
     def save(self, *args, **kwargs):
         super(Profile,self).save(*args, **kwargs)
@@ -21,19 +21,11 @@ class Profile(models.Model):
 
 
 class CSV(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=100)
     pdf = models.FileField(upload_to='csvfiles/')
 
     def __str__(self):
         return f'{self.title}CSV'
-
-    def save(self, *args, **kwargs):
-        super(CSV,self).save(*args, **kwargs)
-
-    def delete(self, *args, **kwargs):
-        self.pdf.delete()
-        super().delete(*args, **kwargs)
 
 
 class CurrentFile(models.Model):
