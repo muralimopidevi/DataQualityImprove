df9405f534ef4bb9f8560d6d75a0263a4a90bd36
Murali Krishna Mopidevi <muralikrishna.mopidevi@gmail.com>
2020-04-13 23:06:39 +0200

    csv preview added

--- dash/views.py
+++ dash/views.py
@@ -1,19 +1,17 @@
 import os
-import plotly.graph_objs as go
-import plotly.offline as opy
-from dash.models import CurrentFile, Prepross
+import pandas as pd
+from django.contrib import messages
+from django.shortcuts import render
 from django.shortcuts import redirect
 from django.views.generic import TemplateView, CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.decorators import login_required
-from .forms import UserUpdateForm, ProfileUpdateForm
-from django.contrib import messages
-from django.shortcuts import render
-import pandas as pd
-from .forms import CSVForm
-from .models import CSV
+import plotly.graph_objs as go
+import plotly.offline as opy
+from dash.models import CurrentFile, Prepross, CSV
+from .forms import UserUpdateForm, ProfileUpdateForm, CSVForm
 
 
 # ------------------ UPLOADING CSV FILE ------------------------- #
@@ -35,10 +33,11 @@ class index(TemplateView):
             context = {}
             context['fileselected'] = list(request.POST.keys())[1]
             context['test'] = 'a'
+            context['title'] = 'Select File'
             return render(request, 'dash/index.html', context)
 
     def __str__(self):
-        return self.name
+        return self.template_name
 
 
 # ------------------ PREPROCESSING  ------------------------- #
@@ -52,24 +51,23 @@ class prepross(TemplateView):
         context = {}
         try:
             file_name = CurrentFile.objects.order_by('-id')[0].filename
-            df = pd.read_csv(os.path.join('Media\csvfiles', file_name))
+            df1 = pd.read_csv(os.path.join('Media\csvfiles', file_name), low_memory=False)
+            df = df1.infer_objects()
             count_nan = len(df) - df.count()
             row_count = df.count()[0]
             file_type = pd.concat([df.dtypes, count_nan, df.nunique()], axis=1)
             file_type.columns = ("Type", "NA count", "Count distinct")
-            context['df'] = df
-            context['vals'] = df.values.tolist()
+            context['DataFrame'] = df
             context['file_type'] = file_type
+            context['title'] = 'Preprocess'
             context['row_count'] = row_count
         except:
             file_name = 'Please select one file'
-
         context['file_name'] = file_name
         return context
 
     def post(self, request, **kwargs):
         if request.method == 'POST':
-
             # Entry to model
             prep_file = Prepross.objects.get_or_create(filename=CurrentFile.objects.order_by('-id')[0].filename,
                                                        coltype=request.POST.getlist('coltype'),
@@ -77,7 +75,6 @@ class prepross(TemplateView):
                                                        missingvalues=request.POST.getlist('missingvalues'),
                                                        trainingset_size=request.POST['trainingset_size'],
                                                        featscaling=request.POST.getlist('featscaling'))
-
             # Get dataframe and change data type
             context = {}
             file_name = CurrentFile.objects.order_by('-id')[0].filename
@@ -85,7 +82,6 @@ class prepross(TemplateView):
             coltype = dict([i.split(':', 1) for i in coltype])
             df = pd.read_csv(os.path.join('Media\csvfiles', file_name), dtype= coltype)
             row_count = df.count()[0]
-
             # Keep only selected columns
             assvar = request.POST.getlist('assvar')
             xcols0 = [s for s in assvar if ":X" in s]
@@ -94,7 +90,6 @@ class prepross(TemplateView):
             ycol = [i.split(':', 1)[0] for i in ycol0]
             cols = xcols + ycol
             df = df[cols]
-
             xcols = ', '.join(xcols)
             ycol = ', '.join(ycol)
             missing = request.POST.getlist('missingvalues')
@@ -103,22 +98,18 @@ class prepross(TemplateView):
             trainingset_s = ', '.join(trainingset_s)
             testset_s = 100 - int(trainingset_s)
             feat =  request.POST['featscaling']
-
             # Taking care of missing data
             if missing == "no":
                 if len(df) != len(df.dropna()):
                     context['selecty'] = 'Your data seem to have Missing Values'
                 else:
                     df = df.dropna()
-
             # Return error if columns are not selected
             if len(ycol0) != 1:
                 context['selecty'] = 'Please select one y variable'
             elif len(xcols0) < 1:
                 context['selecty'] = 'Please select one or more X variables'
-
             else:
-
                 graph = {}
                 for i in df.columns:
                     layout = go.Layout(autosize=False, width=400, height=400,
@@ -130,7 +121,6 @@ class prepross(TemplateView):
                     data = go.Figure(data=[go.Histogram(x=df[i])], layout=layout)
                     graph[i] = opy.plot(data, include_plotlyjs=False, output_type='div')
                 context['graph'] = graph
-
             context['xcols'] = xcols
             context['ycol'] = ycol
             context['missing'] = missing
@@ -142,7 +132,7 @@ class prepross(TemplateView):
             return render(request, 'dash/preprocessing.html', context)
 
     def __str__(self):
-        return self.name
+        return self.template_name
 
 
 # Profile
@@ -159,7 +149,6 @@ def profile(request):
     else:
         uu_form = UserUpdateForm(instance=request.user)
         pp_form = ProfileUpdateForm(instance=request.user.profile)
-
     context = {
         'uuu_form': uu_form,
         'pup_form': pp_form
