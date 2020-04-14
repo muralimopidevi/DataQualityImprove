28494c695b35c16d0e0b78f1ffcb85d8192d93d2
Murali Krishna Mopidevi <muralikrishna.mopidevi@gmail.com>
2020-04-14 23:35:09 +0200

    Sklearn SimpleImputer Done

--- dash/views.py
+++ dash/views.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 from django.contrib import messages
 from django.shortcuts import render
 from django.shortcuts import redirect
@@ -10,6 +11,7 @@ from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.decorators import login_required
 import plotly.graph_objs as go
 import plotly.offline as opy
+from sklearn.impute import SimpleImputer
 from dash.models import CurrentFile, Prepross, CSV
 from .forms import UserUpdateForm, ProfileUpdateForm, CSVForm
 
@@ -27,10 +29,10 @@ class index(TemplateView):
     template_name = 'dash/index.html'
 
     def post(self, request, **kwargs):
+        context = {}
         if request.method == 'POST':
             current_file = CurrentFile(filename=list(request.POST.keys())[1])
             current_file.save()
-            context = {}
             context['fileselected'] = list(request.POST.keys())[1]
             context['test'] = 'a'
             context['title'] = 'Select File'
@@ -44,14 +46,15 @@ class index(TemplateView):
 class prepross(TemplateView):
     template_name = 'dash/preprocessing.html'
     # model = Prepross
-    # fields = ('file_name','coltype','Xvars','Yvar','onehot','featscaling','na_omit')
+    # fields = ('file_name','coltype','Xvars','Yvar','onehot','featscaling','Drop_NA')
 
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
         context = {}
         try:
             file_name = CurrentFile.objects.order_by('-id')[0].filename
-            df1 = pd.read_csv(os.path.join('Media\csvfiles', file_name), low_memory=False)
+            df1 = pd.read_csv(os.path.join('Media\csvfiles', file_name))
+            # Infer Data types
             df = df1.infer_objects()
             count_nan = len(df) - df.count()
             row_count = df.count()[0]
@@ -68,42 +71,55 @@ class prepross(TemplateView):
 
     def post(self, request, **kwargs):
         if request.method == 'POST':
-            # Entry to model
-            prep_file = Prepross.objects.get_or_create(filename=CurrentFile.objects.order_by('-id')[0].filename,
-                                                       coltype=request.POST.getlist('coltype'),
-                                                       assvar=request.POST.getlist('assvar'),
-                                                       missingvalues=request.POST.getlist('missingvalues'),
-                                                       trainingset_size=request.POST['trainingset_size'],
-                                                       featscaling=request.POST.getlist('featscaling'))
             # Get dataframe and change data type
             context = {}
             file_name = CurrentFile.objects.order_by('-id')[0].filename
             coltype = request.POST.getlist('coltype')
             coltype = dict([i.split(':', 1) for i in coltype])
-            df = pd.read_csv(os.path.join('Media\csvfiles', file_name), dtype= coltype)
+            df = pd.read_csv(os.path.join('Media\csvfiles', file_name), low_memory=False, dtype=coltype)
             row_count = df.count()[0]
             # Keep only selected columns
             assvar = request.POST.getlist('assvar')
             xcols0 = [s for s in assvar if ":X" in s]
-            xcols = [i.split(':', 1)[0] for i in xcols0]
+            xcols = [i.split(':', 1)[0] for i in xcols0]  # Select X cols from user form
             ycol0 = [s for s in assvar if ":y" in s]
-            ycol = [i.split(':', 1)[0] for i in ycol0]
-            cols = xcols + ycol
-            df = df[cols]
+            ycol = [i.split(':', 1)[0] for i in ycol0]  # Select y cols from user form
+            cols = xcols + ycol  # combining Both X and Y
+            df = df[cols]  # Creating a DataFrame from combined Cols
             xcols = ', '.join(xcols)
             ycol = ', '.join(ycol)
             missing = request.POST.getlist('missingvalues')
             missing = ', '.join(missing)
+            imputing = request.POST.getlist('imputing')
+            imputing = ', '.join(imputing)
             trainingset_s = request.POST.getlist('trainingset_size')
             trainingset_s = ', '.join(trainingset_s)
             testset_s = 100 - int(trainingset_s)
-            feat =  request.POST['featscaling']
+            feat = request.POST['featscaling']
             # Taking care of missing data
             if missing == "no":
                 if len(df) != len(df.dropna()):
                     context['selecty'] = 'Your data seem to have Missing Values'
                 else:
                     df = df.dropna()
+            if imputing == "mean" or imputing == "median" or imputing == "most_frequent" or imputing == "constant":
+                impute = imputing
+                categorical_columns = []
+                numeric_columns = []
+                for c in df.columns:
+                    if df[c].map(type).eq(str).any():  # check if there are any strings in column
+                        categorical_columns.append(c)
+                    else:
+                        numeric_columns.append(c)
+                # create two DataFrames, one for each data type
+                data_numeric = df[numeric_columns]
+                data_categorical = pd.DataFrame(df[categorical_columns])
+                imp = SimpleImputer(missing_values=np.nan, strategy=impute)
+                data_numeric = pd.DataFrame(imp.fit_transform(data_numeric),
+                                            columns=data_numeric.columns)  # only apply imputer to numeric columns
+                # you could do something like one-hot-encoding of data_categorical here
+                # join the two masked data frames back together
+                df = pd.concat([data_numeric, data_categorical], axis=1)
             # Return error if columns are not selected
             if len(ycol0) != 1:
                 context['selecty'] = 'Please select one y variable'
@@ -129,6 +145,8 @@ class prepross(TemplateView):
             context['feat'] = feat
             context['file_name'] = file_name
             context['row_count'] = row_count
+            context['df'] = df
+
             return render(request, 'dash/preprocessing.html', context)
 
     def __str__(self):
